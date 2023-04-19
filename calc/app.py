93a711fbf52de102d50b9f68ff78cc076f1f0f78
Timothy Sukamtoh <timtoh168@gmail.com>
2023-04-19 12:28:13 -0700

    create all in one and update requirements

--- calc/app.py
+++ calc/app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 
 @app.get("/add")
 def add_nums():
+    """Returns string after performing operations.add"""
+
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     sum = add(a, b)
@@ -17,6 +19,8 @@ def add_nums():
 
 @app.get("/sub")
 def sub_nums():
+    """Returns string after performing operations.sub"""
+
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     return str(sub(a, b))
@@ -24,6 +28,8 @@ def sub_nums():
 
 @app.get("/mult")
 def mult_nums():
+    """Returns string after performing operations.mult"""
+
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     return str(mult(a, b))
@@ -31,6 +37,17 @@ def mult_nums():
 
 @app.get("/div")
 def div_nums():
+    """Returns string after performing operations.div"""
+
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     return str(div(a, b))
+
+@app.get("/math/<operation>")
+def all_in_one(operation):
+    """Returns string after performing specific operation"""
+    OPERATIONS = {"add":add, "sub":sub, "mult":mult, "div":div}
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return str(OPERATIONS[operation(a, b)])
